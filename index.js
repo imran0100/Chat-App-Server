284760384be6622d908926568b4fc7387af6fd75
Imran Ansari <i4imran322@gmail.com>
2023-03-02 23:32:13 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("file", (data) => {
+    console.log(`File from ${socket.username}: ${data.filename}`);
+    const filepath = path.join(__dirname, "public", "uploads", data.filename);
+    const fileStream = fs.createWriteStream(filepath);
+    fileStream.write(data.file);
+    fileStream.end();
+    socket.broadcast.emit("file", {
+      username: socket.username,
+      filename: data.filename,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
